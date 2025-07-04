a5c653beb2dd26860880f4f8df14578da52e4cff
Laib Ahmed Yacine <ahmed.laib@eadn.dz>
2025-07-04 17:37:04 +0100

    Initial commit

---
+++ src/main/groovy/dz/eadn/readerpro/ReaderProApplication.groovy
@@ -0,0 +1,13 @@
+package dz.eadn.readerpro
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ReaderProApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ReaderProApplication, args)
+    }
+
+}
---
+++ src/test/groovy/dz/eadn/readerpro/ReaderProApplicationTests.groovy
@@ -0,0 +1,13 @@
+package dz.eadn.readerpro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReaderProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
