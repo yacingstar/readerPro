b37c8ff0a00a0410cfbb08a26b3e629257d5c3a7
Laib Ahmed Yacine <ahmed.laib@eadn.dz>
2025-07-04 21:34:14 +0100

    initial Cmmit

---
+++ src/main/java/dz/eadn/readerpro/ReaderProApplication.java
@@ -0,0 +1,13 @@
+package dz.eadn.readerpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderProApplication.class, args);
+	}
+
+}
